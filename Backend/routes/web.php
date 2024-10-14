0077ccc6bad75546d905597ceedfc368f050b63f
Joan Aquino <joan.aquino.vasquez@gmail.com>
2024-10-14 17:30:21 -0500

    Autentificacion Google

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -1,7 +1,38 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/google-auth/redirect', function () {
+    return Socialite::driver('google')->redirect();
+});
+
+// Callback para manejar la respuesta de Google
+Route::get('/google-auth/callback', function () {
+    $googleUser = Socialite::driver('google')->stateless()->user();
+
+    // Aquí puedes encontrar o crear el usuario en tu base de datos
+    $user = User::where('email', $googleUser->getEmail())->first();
+
+    if (!$user) {
+        // Si el usuario no existe, crea uno nuevo
+        $user = User::updateOrCreate([
+            'name' => $googleUser->getName(),
+            'email' => $googleUser->getEmail(),
+        ]);
+    }
+
+    // Inicia sesión al usuario
+    Auth::login($user);
+
+      // Redirige a la aplicación frontend después de iniciar sesión
+      return redirect('http://localhost:5173/Inicio'); // Ajusta la URL según tu aplicación React
 });
