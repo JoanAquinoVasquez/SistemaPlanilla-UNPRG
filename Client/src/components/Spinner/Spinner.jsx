4f9f141258e14397ff796b07cf1ff3d27c0cf302
juanchiscol <chiscolpatazcajuandavid@gmail.com>
2024-11-06 11:48:11 -0500

    Eliminar archivo CSS del Spinner y actualizar el componente para usar clases de Tailwind CSS

--- Client/src/components/Spinner/Spinner.jsx
+++ Client/src/components/Spinner/Spinner.jsx
@@ -1,12 +1,16 @@
 import { Spinner as NextUISpinner } from "@nextui-org/react"; // Cambia el nombre aquí
-import "./Spinner.css";
+import PropTypes from "prop-types";
 
 const Spinner = ({label}) => {
   return (
-    <div className="spinner-container">
+    <div className="flex justify-center items-center h-screen w-screen">
       <NextUISpinner size="lg" label={label} />
     </div>
   );
+};
+
+Spinner.propTypes = {
+  label: PropTypes.string,
 };
 
 export default Spinner;
