38f4aa2ba85b6f6f591220e054398cdef9786b46
Joan Aquino <joan.aquino.vasquez@gmail.com>
2024-11-13 10:53:02 -0500

    Fx correccion SessionManager

--- Client/src/SessionManager.jsx
+++ Client/src/SessionManager.jsx
@@ -1,46 +1,99 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { handleLogout } from "./components/Logout/Logout";
 import ModalSessionExpiration from "./components/SessionModal/SessionModal";
 
 const SessionManager = ({ children }) => {
+  const navigate = useNavigate();
+
+  // Tiempos de control
+  const INACTIVITY_LIMIT = 15 * 60 * 1000; // 15 minutos
+  const WARNING_TIME_BEFORE_EXPIRATION = 4 * 60 * 1000; // 4 minutos antes de expiración
+  const AUTO_LOGOUT_TIME_BEFORE_EXPIRATION = 1 * 60 * 1000; // 1 minuto antes de expiración
+  const INACTIVITY_MODAL_CLOSE_TIME = 3 * 60 * 1000; // 3 minutos para responder al modal
+
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [modalType, setModalType] = useState(""); // Tipo de modal ("expiracion" o "inactividad")
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [expiration, setExpiration] = useState(
-    sessionStorage.getItem("expiration")
+    parseInt(sessionStorage.getItem("expiration"), 10)
   );
-  const navigate = useNavigate();
+  const [inactivityCounter, setInactivityCounter] = useState(INACTIVITY_LIMIT / 1000); // Contador de inactividad en segundos
+  const [modalResponseCounter, setModalResponseCounter] = useState(INACTIVITY_MODAL_CLOSE_TIME / 1000); // Contador para responder al modal en segundos
 
-  // Tiempo de inactividad y otros tiempos de control
-  const INACTIVITY_LIMIT = 15 * 60 * 1000; // 15 minutos
-  const WARNING_TIME_BEFORE_EXPIRATION = 3 * 60 * 1000; // 3 minutos antes de expiración
-  const AUTO_LOGOUT_TIME_BEFORE_EXPIRATION = 1 * 60 * 1000; // 1 minuto antes de expiración
-  const INACTIVITY_MODAL_CLOSE_TIME = 2 * 60 * 1000; // 2 minutos para responder al modal
-  let inactivityTimeout;
-  let inactivityModalTimeout; // Tiempo para cierre automático en modal de inactividad
+  // Refs para los temporizadores
+  const inactivityTimeoutRef = useRef(null);
+  const inactivityCounterIntervalRef = useRef(null);
+  const inactivityModalTimeoutRef = useRef(null);
+  const expirationIntervalRef = useRef(null);
 
   // Función para reiniciar el temporizador de inactividad
   const resetInactivityTimeout = () => {
-    if (inactivityTimeout) clearTimeout(inactivityTimeout);
-    inactivityTimeout = setTimeout(() => {
-      // Solo mostrar el modal de inactividad si el modal de expiración no está activo
-      if (!isModalVisible) {
-        setModalType("inactividad");
-        setIsModalVisible(true);
+    setInactivityCounter(INACTIVITY_LIMIT / 1000); // Reinicia el contador de inactividad
+    if (inactivityTimeoutRef.current) clearTimeout(inactivityTimeoutRef.current);
+    if (inactivityCounterIntervalRef.current) clearInterval(inactivityCounterIntervalRef.current);
 
-        // Iniciar temporizador para cierre automático en caso de inactividad
-        inactivityModalTimeout = setTimeout(() => {
-          setIsModalVisible(false);
-          setIsAuthenticated(false);
-          handleLogout(
-            navigate,
-            "Su sesión ha caducado debido a inactividad, por favor vuelva a iniciar sesión"
-          );
-        }, INACTIVITY_MODAL_CLOSE_TIME);
-      }
+    // Inicia el intervalo del contador de inactividad
+    inactivityCounterIntervalRef.current = setInterval(() => {
+      setInactivityCounter((prev) => {
+        if (prev > 0) return prev - 1;
+        clearInterval(inactivityCounterIntervalRef.current);
+        return 0;
+      });
+    }, 1000);
+
+    // Configura el tiempo límite de inactividad
+    inactivityTimeoutRef.current = setTimeout(() => {
+      setModalType("inactividad");
+      setIsModalVisible(true);
+      setModalResponseCounter(INACTIVITY_MODAL_CLOSE_TIME / 1000);
+
+      // Temporizador de cierre automático del modal si no se responde
+      inactivityModalTimeoutRef.current = setTimeout(() => {
+        setIsModalVisible(false);
+        setIsAuthenticated(false);
+        handleLogout(
+          navigate,
+          "Su sesión ha caducado debido a inactividad, por favor vuelva a iniciar sesión"
+        );
+      }, INACTIVITY_MODAL_CLOSE_TIME);
     }, INACTIVITY_LIMIT);
+  };
+
+  // Configura el chequeo de expiración y reinicia el intervalo
+  const setupExpirationCheck = () => {
+    if (expirationIntervalRef.current) clearInterval(expirationIntervalRef.current);
+
+    expirationIntervalRef.current = setInterval(() => {
+      const currentTime = new Date().getTime();
+      const warningTime = expiration - WARNING_TIME_BEFORE_EXPIRATION;
+      const autoLogoutTime = expiration - AUTO_LOGOUT_TIME_BEFORE_EXPIRATION;
+
+      if (currentTime >= warningTime && currentTime < expiration && (!isModalVisible || modalType === "inactividad")) {
+        setModalType("expiracion");
+        setIsModalVisible(true);
+        clearTimeout(inactivityModalTimeoutRef.current);
+      }
+
+      if (currentTime >= autoLogoutTime) {
+        setIsModalVisible(false);
+        setIsAuthenticated(false);
+        handleLogout(
+          navigate,
+          "Su sesión ha caducado, por favor vuelva a iniciar sesión"
+        );
+      }
+    }, 1000);
+  };
+
+  // Función para cerrar el modal y reiniciar el temporizador de inactividad
+  const handleModalClose = () => {
+    setIsModalVisible(false);
+    clearTimeout(inactivityModalTimeoutRef.current);
+
+    // Reinicia el temporizador de inactividad después de cerrar el modal
+    resetInactivityTimeout(); // Asegura que el contador se reinicie y vuelva a mostrarse al llegar a 0
   };
 
   // Escucha eventos de usuario para detectar actividad
@@ -50,13 +103,14 @@ const SessionManager = ({ children }) => {
       window.addEventListener(event, resetInactivityTimeout)
     );
 
-    // Limpiar eventos y temporizadores al desmontar
     return () => {
       events.forEach((event) =>
         window.removeEventListener(event, resetInactivityTimeout)
       );
-      clearTimeout(inactivityTimeout);
-      clearTimeout(inactivityModalTimeout); // Limpia el temporizador de cierre automático del modal
+      clearTimeout(inactivityTimeoutRef.current);
+      clearTimeout(inactivityModalTimeoutRef.current);
+      clearInterval(expirationIntervalRef.current);
+      clearInterval(inactivityCounterIntervalRef.current);
     };
   }, []);
 
@@ -67,85 +121,38 @@ const SessionManager = ({ children }) => {
     }
   }, [expiration]);
 
+  useEffect(() => {
+    if (isAuthenticated && expiration) {
+      resetInactivityTimeout();
+      setupExpirationCheck();
+    }
+    return () => clearInterval(expirationIntervalRef.current);
+  }, [isAuthenticated, expiration]);
+
   const verifyAuth = async () => {
     if (!isAuthenticated) return;
     try {
-      const response = await axios.get("/check-auth", {
-        withCredentials: true,
-      });
-      if (response.data.authenticated) {
-        setIsAuthenticated(true);
-      } else {
-        setIsAuthenticated(false);
-      }
+      const response = await axios.get("/check-auth", { withCredentials: true });
+      setIsAuthenticated(response.data.authenticated);
     } catch (error) {
       setIsAuthenticated(false);
     }
   };
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (isAuthenticated) {
-        verifyAuth();
-      } else {
-        clearInterval(interval);
-      }
-    }, 1 * 60 * 1000); // Verificación cada minuto
-
-    return () => clearInterval(interval);
-  }, [isAuthenticated]);
-
-  // Control de expiración y advertencia
-  useEffect(() => {
-    if (!isAuthenticated || !expiration) return;
-
-    const warningTime = expiration - WARNING_TIME_BEFORE_EXPIRATION;
-    const autoLogoutTime = expiration - AUTO_LOGOUT_TIME_BEFORE_EXPIRATION;
-
-    const checkSession = () => {
-      const currentTime = new Date().getTime();
-
-      // Muestra el modal de expiración del token y desactiva el de inactividad
-      if (
-        currentTime >= warningTime &&
-        currentTime < expiration &&
-        (!isModalVisible || modalType === "inactividad") // Prioriza la expiración sobre inactividad
-      ) {
-        setModalType("expiracion");
-        setIsModalVisible(true);
-        clearTimeout(inactivityModalTimeout); // Desactiva el temporizador de inactividad
-      }
-
-      // Cierra la sesión automáticamente 30 segundos antes de la expiración del token
-      if (currentTime >= autoLogoutTime) {
-        setIsModalVisible(false);
-        setIsAuthenticated(false);
-        handleLogout(
-          navigate,
-          "Su sesión ha caducado, por favor vuelva a iniciar sesión"
-        );
-      }
-    };
-
-    const interval = setInterval(checkSession, 1000);
-    return () => clearInterval(interval);
-  }, [isAuthenticated, expiration, navigate, isModalVisible, modalType]);
-
   // Función para manejar la renovación del token
   const handleKeepSession = async () => {
     try {
-      const response = await axios.post(
-        "/refresh-token",
-        {},
-        { withCredentials: true }
-      );
+      const response = await axios.post("/refresh-token", {}, { withCredentials: true });
       const newExpiration = response.data.expiration * 1000;
       sessionStorage.setItem("expiration", newExpiration);
       setExpiration(newExpiration);
-      setIsModalVisible(false);
       setIsAuthenticated(true);
-      resetInactivityTimeout(); // Reinicia el temporizador de inactividad
-      clearTimeout(inactivityModalTimeout); // Cancela el temporizador de cierre automático
+      setIsModalVisible(false);
+
+      clearTimeout(inactivityTimeoutRef.current);
+      clearTimeout(inactivityModalTimeoutRef.current);
+      resetInactivityTimeout();
+      setupExpirationCheck();
     } catch (error) {
       setIsAuthenticated(false);
       handleLogout(
@@ -160,10 +167,10 @@ const SessionManager = ({ children }) => {
       {isModalVisible && (
         <ModalSessionExpiration
           isOpen={isModalVisible}
-          onClose={() => setIsModalVisible(false)}
+          onClose={handleModalClose} // Reinicia el temporizador al cerrar el modal
           onKeepSession={handleKeepSession}
           onLogout={() => handleLogout(navigate)}
-          type={modalType} // Pasa el tipo de modal
+          type={modalType}
         />
       )}
       {children}
